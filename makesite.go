714eca6a1acb3c20c668c1f39998e0969b1e69ca
Paul Fletes <paul.fletes@gmail.com>
2024-04-03 17:36:10 -0500

    v1.0 task 2 complete

--- makesite.go
+++ makesite.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Path to file
+	filePath := "first-post.txt"
+
+	// Read file
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		// A common use of `panic` is to abort if a function returns an error
+		// value that we don’t know how to (or want to) handle. This example
+		// panics if we get an unexpected error when creating a new file.
+		panic(err)
+	}
+
+	// Print file contents
+	fmt.Println(string(data))
 }
